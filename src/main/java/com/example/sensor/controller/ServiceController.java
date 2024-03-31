a2b1595f4aff11cc3febac7c6caf6f0b77f8c19b
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-03-31 16:08:02 +0900

    feat: sensor data parsing

--- src/main/java/com/example/sensor/controller/ServiceController.java
+++ src/main/java/com/example/sensor/controller/ServiceController.java
@@ -2,7 +2,11 @@ package com.example.sensor.controller;
 
 import com.example.sensor.dto.Payload;
 import com.example.sensor.dto.SensorData;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.sensor.model.Gravity;
+import com.example.sensor.producer.SensorProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +16,29 @@ import java.util.List;
 @RestController
 public class ServiceController {
 
+    @Autowired
+    SensorProducer sensorProducer;
+
+    ObjectMapper objectMapper;
+
     @PostMapping("/")
     public void run(@RequestBody SensorData data)
     {
         List<Payload> payloads = data.getPayload();
+        ObjectMapper objectMapper = new ObjectMapper();
         for(Payload payload : payloads)
         {
-            System.out.println(payload);
+            if(payload.getName().equals("gravity"))
+            {
+                try {
+                    String respData = objectMapper.writeValueAsString(payload.getValues());
+                    Gravity gravity = objectMapper.readValue(
+                            respData, Gravity.class);
+                    sensorProducer.sendGravityData(gravity);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 
