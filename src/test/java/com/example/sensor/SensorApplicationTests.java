14fd827f09886e90320f7ba927a8aec2e4224b27
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-04-07 16:38:36 +0900

    feat: schema registry data 전송 test

--- src/test/java/com/example/sensor/SensorApplicationTests.java
+++ src/test/java/com/example/sensor/SensorApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.sensor;
 
+import com.example.sensor.model.Gravity;
+import com.example.sensor.producer.SensorProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SensorApplicationTests {
 
+	SensorProducer sensorProducer = new SensorProducer();
 	@Test
 	void contextLoads() {
+		Gravity gravity = Gravity.builder().x((float)1.123).y((float)1.223).z((float)1.333).build();
+		sensorProducer.sendGravityData(gravity);
 	}
 
 }
